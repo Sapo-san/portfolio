64098d8e21444dc4518a395773877c5860279ab8
Sapo-san <cristobal.herreros@gmail.com>
2022-07-27 21:03:01 -0400

    mejor implementacion de navbar (burger menu), añadidos componentes about y utility

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import './Nav.scss'
 
 const Nav = () => {
 
+    const [showNavInMobile, setShowNavInMobile] = useState(false);
+    const [navLinkContainerClass, setNavLinkContainerClass] = useState('burger-container hide')
+    const [navbarClass, setNavbarClass] = useState('Navbar flex row centered')
+
     const location = useLocation()
+
+    useEffect(() => {
+        
+        if (showNavInMobile) {
+            setNavLinkContainerClass('navlink-container')
+            setNavbarClass('Navbar flex row centered fullOpacity')
+
+        } else {
+            setNavLinkContainerClass('navlink-container hide')
+            setNavbarClass('Navbar flex row centered')
+
+        }
+
+    }, [showNavInMobile])
+    
+
 
     if (location.pathname === '/') {
         return null;
     }
 
     return (
-        <nav className='Navbar flex row centered'>
-            <NavLink className="NavLink" to="/">Welcome</NavLink>
-            <NavLink className="NavLink" to="/home">Home</NavLink>
+        <nav className={navbarClass}>
+            <div className="burger-container" >
+                <div className='burger-limit' onClick={() => {
+                setShowNavInMobile(!showNavInMobile)
+                 }}>
+                    <div className='burger-line'></div>
+                    <div className='burger-line'></div>
+                    <div className='burger-line'></div>
+                </div>
+            </div>
+
+            <div className={navLinkContainerClass}>
+                <NavLink className="NavLink" to="/" onClick={() => {
+                    setShowNavInMobile(false)
+                }}>Welcome</NavLink>
+                <NavLink className="NavLink" to="/home" onClick={() => {
+                    setShowNavInMobile(false)
+                }}>Home</NavLink>
+                <NavLink className="NavLink" to="/about" onClick={() => {
+                    setShowNavInMobile(false)
+                }}>About</NavLink>
+                <NavLink className="NavLink" to="/utility" onClick={() => {
+                    setShowNavInMobile(false)
+                }}>Utility</NavLink>
+            </div>
+
+            
         </nav>
   )
 }
---
+++ src/components/Utility/Utility.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Utility = () => {
+  return (
+    <div className='slide flex centered column'>Utility</div>
+  )
+}
+
+export default Utility
