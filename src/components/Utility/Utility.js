32a67f08175bb079c4eb23873749eaccb774c41b
Sapo-san <cristobal.herreros@gmail.com>
2022-08-03 19:07:12 -0400

    fixed overflow ignoring padding in cheatsheet buttons

--- src/components/Utility/Utility.js
+++ src/components/Utility/Utility.js
@@ -19,18 +19,20 @@ const Utility = () => {
         <div className='cheatsheet-selector'>
           {/* Cheatsheet selector */}
           <input className="cheatsheets-search" type="text" placeholder='🔎 Search Not functional yet'></input>
-          <div className='avaible-cheatsheets'>
-            {avaibleCheatsheets.map((ac => {
-              return <div
-              key={ac}
-              className="avaible-cheatsheet-button"
-              onClick={(e) => {
-                setCurrentCheatsheet(e.target.innerText)
-              }}>
-                {ac}
-              </div>
-            }))}
-          </div>
+          <div className='avaible-cheatsheets-container'>
+            <div className='avaible-cheatsheets'>
+              {avaibleCheatsheets.map((ac => {
+                return <div
+                key={ac}
+                className="avaible-cheatsheet-button"
+                onClick={(e) => {
+                  setCurrentCheatsheet(e.target.innerText)
+                }}>
+                  {ac}
+                </div>
+              }))}
+            </div>
+          </div> 
         </div>
         <div className='cheatsheet-container'>
           {/* Displayed Cheatsheet */}
