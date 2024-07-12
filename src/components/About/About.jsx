158fc891ddaf468a01480d5e056cdead29ff48ec
Cristobal Herreros <cristobal.herreros@gmail.com>
2024-07-11 22:54:31 -0400

    maintenance and updates

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+//import React from 'react'
 import './about.scss'
 
 const About = () => {
@@ -9,20 +9,42 @@ const About = () => {
           <h1 className='about-title'>About me</h1>
           <h2 className='about-title'>Previous Working Experiences</h2>
           <p>
-            In this section I talk about my previous working experiences and other stuff that might be relevant.
+            Here&apos;s a list of things I&apos;ve worked at as a freelancer. Most of them are landing pages with a contact form for small businesses.
           </p>
-          <h2>Freelancing:</h2>
+          <p>
+            The list is not that long since I actually found a fulltime job and I don&apos;t have much time to freelance on the side
+          </p>
+          <h2>Behold! My previous freelancing experience:</h2>
           <ul className='about-pwe-list'>
-            <li> <b><a href="https://www.vlabogadas.cl">vlabogadas.cl</a> [August 2022 - Current Day]</b>: Made their website with React.js and using S3 + Cloudfront for their deploy. Also helped them set up Google Workspace.</li>
-            <li> <b><a href="https://www.constructoravenus.cl/">constructoravenus.cl</a> [November 2021 - December 2021]</b>: Made their website with React.js and used S3 + Cloudfront for their deploy. Also helped them set up Google Workspace.</li>
-            <li> <b><a href="https://www.irricoach.cl/">irricoach.cl</a> [March 2021 - Current Day]</b>: I do monthly maintenance to their site. </li>
-            <li> <b><a href="https://lascardas.cl/">lascardas.cl</a> [January 2021 - February 2021]</b>: Helped them move from their old site to their current one (which is on Shoppify) and migrated their mail accounts to Google Workspace.</li>
+            <li>
+              <b><a href="https://www.cirugiadelaobesidad.cl">cirugiadelaobesidad.cl</a> [November 2023 - April 2024]</b>: Vue + Express combo. Deployed on AWS with S3 & Cloudfront.
+            </li>
+            <li>
+              <b><a href="https://www.vlabogadas.cl">vlabogadas.cl</a> [August 2022 - Current Day]</b>: React + Express combo. Deployed on AWS. A complete redesign of the site is pending 👀
+            </li>
+            <li>
+              <b><s>constructoravenus.cl</s> [November 2021 - December 2021]</b>: <s>React + Express combo. Deployed on AWS with S3 & Cloudfront.</s> Their site is no longer available 🤷
+            </li>
+            <li>
+              <b><a href="https://www.irricoach.cl/">irricoach.cl</a> [March 2021 - Current Day]</b>: Wordpress. I only do monthly maintenance to their site. This wasn&apos;t made by me
+            </li>
+            <li>
+              <b><a href="https://lascardas.cl/">lascardas.cl</a> [January 2021 - February 2021]</b>: Helped them migrate from their old site to their current one (which is on Shoppify) and migrated their mail accounts to Google Workspace.
+            </li>
           </ul>
           <h2>Other Experiences:</h2>
           <p>
-            I also was part of the Teaching assistant team for Computer&apos;s Architecture course back in university.
+            I was part of the Teaching assistant team for Computer&apos;s Architecture course back in uni.
             I worked as Lab and Classes asisstant for five semesters (2019 - 2021), mainly grading student&apos;s tests,
             doing Lab lectures and helping them both in person and remotely. We worked with Vivado Software, the Basys3 Artix-7 FPGA and VHDL.
+          </p>
+          <p>
+            Last but not least and also while in uni, I volunteered for a while as betatester
+            for <a href="https://www.moddb.com/mods/swgl"> Star Wars Galactic legacy </a>,
+            a fanmade expansion for a Star Wars PC Videogame. 
+            I had to find bugs in the mod and report them to the modding team so they could be
+            fixed ASAP. It was a short but VERY rewarding experience since I love the game
+            and the community is very welcoming and dedicated to the mod.
           </p>
         </div>
       </div>
--- src/components/Welcome/welcome.jsx
+++ src/components/Welcome/welcome.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+//import React from 'react'
 import { useRef } from 'react'
 import { useNavigate } from "react-router-dom";
 
 import './welcome.scss'
-import timberli from '../../img/timberli.png'
-import videogamepi from '../../img/videogameapp.png'
-import profilepic from '../../img/profilepic.webp'
+
+import profilepic from '../../img/profilepic.png'
 import { Link } from 'react-router-dom'
 import englishCv from '../../files/cv_en.pdf';
 
@@ -35,7 +34,7 @@ const Welcome = () => {
         <h2 className='welcome-subtitle'>How can I help you?</h2>
 
         <div className='welcome-buttons'>
-          <button onClick={() => PortfolioRef.current.scrollIntoView()}> Check Portfolio </button>
+          <button onClick={() => PortfolioRef.current.scrollIntoView()}> About me </button>
           <button onClick={() => {
             // Descargar PDF
             const link = document.createElement('a');
@@ -49,72 +48,87 @@ const Welcome = () => {
 
       {/* Portfolio */}
       <div ref={PortfolioRef} className='portfolio slide flex'>
-        {/* Projectos */}
-        <div className='container flex column portfolio-section'>
-          <h1 className='portfolio-title'>Projects:</h1>
-          <p>
-            These are my finished projects:
-          </p>
-            {/* Project #2 */}
-            <div className="project flex row centered">
-              <div className="project-txt">
-                <p><b>Videogame App - Henry Bootcamp Individual Project:</b> I made this app to practice
-                all of what I learned in Henry Bootcamp in one single project. It&apos;s a simple app that consumes
-                RAWG API, it can sort, filter and create Videogame Titles and displays them in a simple UI. Here&apos;s
-                the <a href="https://github.com/Sapo-san/PI-Videojuegos">Link to the repo</a>.</p>
-              </div>
-              <img className="project-img" src={videogamepi} alt="timberli-img"></img>  
-            </div>
-            {/* Project #1 */}
-            <div className='project flex row centered'>
-              <img className="project-img" src={timberli} alt="timberli-img"></img>
-              <div className="project-txt">
-                <p><b>Timberli - Henry Bootcamp Group Project:</b> Timberli is an app where Jr developers
-                can upload their projects to build their portfolio, be more visible to recruiters and
-                also receive feedback on their projects. Our team worked on Timberli for a
-                month. <a href="https://proyecto-final-orcin-seven.vercel.app/">Check it out, since its already deployed.</a></p>
-              </div>
-            </div>
-          <p>
-          If you want to see other projects I&apos;m currently working on (but at a slow pace, since I&apos;m a fulltime employee), you can check out my <a href="https://github.com/Sapo-san">Github account</a>.
-          </p>
-        </div>
+        {/* Proyectos */}
 
         {/* Sobre Mi */}
         
         <div className='portfolio-section container flex column'>
-          <h1 className='portfolio-title'>About me:</h1>
+          {/* <h1 className='portfolio-title'>About me:</h1> */}
 
-          <div className='flex row centered profile'>
+          <div className='flex row centered profile'>               
+            {/* Profile Image */}
             <img className='profilepic' src={profilepic} alt="profilepic"></img>
+            {/* Profile Text */}
             <div className='profile-text'>
+              
+              {/* My name and job title */}
               <p className='profile-name'>
                 Cristóbal Herreros Viviani
+              </p>     
+              <p className="profile-subtitle">
+                Fullstack Web Developer
               </p>
-              <p>
-                {getAge("07/04/1997")} years old, Chilean 🇨🇱, Fullstack Web Developer.
+              <p className="profile-subsubtitle">
+                (basically jack of all trades, master of none)
               </p>
-              <p>
-                Currently employed at <a href="https://evoluciona.cl/">Evoluciona Chile</a> (since September 5th, 2022) working as external employee for <a href="https://www.linkedin.com/company/entel/">Entel</a> 
+
+              {/* Me talking about myself */}
+              <p className='aboutme-p1'>
+                Hello there! In this section I&apos;ll talk a bit about myself.
+                For starters, I&apos;m {getAge("07/04/1997")} years old and I come
+                from Chile 🇨🇱
+              </p>
+              <p className='aboutme-p1'>
+                I&apos;m currently working at
+                <a href="https://www.linkedin.com/company/entel/"> Entel </a>
+                (since April 22nd, 2024) as an internal collaborator and Developer.
+              </p>
+
+              <p className='aboutme-p1'>
+                My role is to implement infrastructure and automate processes in
+                the cloud to support our B2B Business Analytics team.
+              </p>
+
+              <p className='aboutme-p1'>
+                It is also my job to give support to legacy applications and maintain
+                their operational continuity while we migrate to a newer tech stack.
+              </p>
+
+              <p className='aboutme-p1'>
+                Some personal stuff about me is that I really like frogs 🐸, reading
+                books 📚 and playing 🎮 videogames. 
+              </p>
+
+              <p className='aboutme-p1'>
+                I&apos;m also into fencing (both ⚔️ HEMA
+                longsword and lightsaber fencing) and I really like 🖥️ computers
+                and everything related to them.
+              </p>
+              
+              <p className='aboutme-p1'>
+                Previous working experiences? Before my current job I worked for 
+                <a href="https://evoluciona.cl/"> Evoluciona Chile </a> for a year
+                and a half as Software Engineer and before that I did a bit of freelancing here and
+                there (click on &quot;Read more&quot; below to see a not-that-long-but-complete list of things).
+              </p>
+
+              <p className='aboutme-p1'>
+                Also, I was part of the teaching assistant team for the Computer&apos;s Architecture
+                course back in university. 
               </p>
             </div>
           </div>
-          <p className='aboutme-p1'>
-            I like 🐸 frogs. And 🌌 space. Also I like playing 🎮 videogames,
-            ⚔️ HEMA longsword fencing and I really like  🖥️ computers
-            and everything related to them. 
-            <br/>
-            <br/>
-            Previous experiences? Before my current job I did a bit of freelancing here and there (click on &quot;Read more&quot; below to see a complete list), 
-            also I was teaching assistant for the Computer&apos;s Architecture course back in university.
-          </p>
+          
           <p className='aboutme-p2'>
-          <h2>My current Tech Stack is:</h2>
-          <ul>
-            <li>Programming Languages: <b>Java & Javascript</b> (Using both at my current job), Python, SQL (Postgres and MySQL)</li>
-            <li>Libraries / Frameworks: <b>Vue2 & Java Springboot</b> (Using both at my current job), Vue3, React.js, Redux, Node.js, Express.js, Sequelize</li>
-            <li>Agile Methodologies: Scrum, Kanban</li>
-          </ul>
+            <h2>My Tech Stack is:</h2>
+              <ul>
+                <li>Javascript, Python, Php, Java, SQL (Postgres and MySQL)</li>
+                <li>Vue 3, Vue 2, Java Springboot, React, Redux, Node.js, Express.js, Sequelize</li>
+                <li>Google Cloud: Compute Engine, Cloud Functions, Big Query, Cloud Storage, Looker Studio, Google DNS, Cloud Scheduler...</li>
+                <li>Amazon Web Services: EC2, S3, Route 53, Amazon Cloudfront.</li>
+                <li>Scrum, Kanban using Jira</li>
+                <li>Other tools: Git, Cloud Console, etc...</li>
+              </ul>
           </p>
 
           <Link className="welcome-link readMoreButton" to="/about">Read More</Link>
